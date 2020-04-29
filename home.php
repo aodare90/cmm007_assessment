8490528e12e2546ad35a965137ecc095c011af23
aodare90 <1909767@rgu.ac.uk>
2020-04-29 22:52:46 +0100

    CMM007

---
+++ home.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>ReviewMe! | Peer Assessment</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+<body>
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+    <div class="container"><a class="navbar-brand logo" href="home.php"><span style="font-family: cursive;">Review</span>Me</a>
+        <div class="collapse navbar-collapse" id="navcol-1">
+            <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item" role="presentation"><a class="nav-link active" href="home.php">Home</a></li>
+                <li class="nav-item" role="presentation"><a class="nav-link" href="about-us.html">About Us</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<main class="page landing-page">
+    <section class="clean-block clean-hero" style="background-image:url(&quot;assets/img/tech/home.jpg&quot;);color:rgba(9, 162, 255, 0.85);">
+        <div class="text">
+            <h2>Peer Assesment</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in, mattis vitae leo.</p><a href="login.php"><button class="btn btn-outline-light btn-lg" type="button">Log In</button></a></div>
+    </section>
+    <section class="clean-block features">
+        <div class="container">
+            <div class="block-heading">
+                <h2 class="text-info">Features</h2>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-5 feature-box"><i class="icon-star icon"></i>
+                    <h4>Bootstrap 4</h4>
+                    <p>Made with Bootsrap 4 to create a beautiful, seamless and stress-free experience.</p>
+                </div>
+                <div class="col-md-5 feature-box"><i class="icon-pencil icon"></i>
+                    <h4>Edit</h4>
+                    <p>Leave feedback for follow members in your group.</p>
+                </div>
+                <div class="col-md-5 feature-box"><i class="icon-screen-smartphone icon"></i>
+                    <h4>Responsive</h4>
+                    <p>Whether you are home or away, <span style="font-family: cursive;">Review</span>Me is available on any device to suit your needs.</p>
+                </div>
+                <div class="col-md-5 feature-box"><i class="icon-refresh icon"></i>
+                    <h4>Sync</h4>
+                    <p>All feedback is stored for Instructors to see at the end of the group work. Feedback is confidential.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+</footer>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+<script src="assets/js/smoothproducts.min.js"></script>
+<script src="assets/js/theme.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,115 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+session_start();
+session_destroy();
+$pageTitle = "Login";
+require_once("class/functions.php");
+
+$msg = "";
+$status = "";
+$content = "";
+
+if (isset($_COOKIE['username']) && $_COOKIE['password']) {
+    loginFunction($_COOKIE['username'],$_COOKIE['password']);
+}
+
+if (isset($_POST['submitForm'])) {
+    $usernm = sanitize::clean($_POST['username']);
+    $password = sanitize::clean($_POST['password']);
+
+    if ($usernm!="" && $password!="") {
+        if (!empty($_POST['remember_me'])) {
+            setcookie("username",$usernm,time()+3600);
+            setcookie("password",$password, time()+3600);
+        }
+        else{
+            setcookie("username",'');
+            setcookie("password",'');
+        }
+        loginFunction($usernm,$password);
+    }else{
+        $status = "error";
+        $msg = "Please enter Username and Password!";
+    }
+}
+function loginFunction($username,$password){
+    $users = new users();
+    $dataArray = array("username"=>$username,"password"=>$password);
+    $result =  $users->login($dataArray);
+
+    if ($result['status']=="success"){
+        session_start();
+        $_SESSION['memberLogin'] = 'assessment2020';
+        $_SESSION['myUserId'] = $result["id"];
+        $_SESSION['myFirstname'] = $result["firstname"];
+        $_SESSION['myLastname'] = $result["lastname"];
+        $_SESSION["myEmail"] = $result["email"];
+        $_SESSION["myPhoto"] = $result["photo"];
+        $_SESSION['myAboutme'] = $result['aboutme'];
+        $_SESSION['myRole'] = $result['role'];
+        header("location:dashboard.php");
+    }
+    else{
+        $status = $result["status"];
+        $msg = $result["message"];
+    }}
+?>
+<head>
+    <link rel="icon" type="image/png" href="assets/img/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <div class="login100-pic js-tilt" data-tilt>
+                <img src="assets/img/img-01.png" alt="IMG">
+            </div>
+            <form class="login100-form validate-form" method="POST" action="login.php">
+                <span class="login100-form-title"> LOGIN </span>
+                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <input class="input100" type="email" name="username" placeholder="Username">
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-envelope" aria-hidden="true"></i>
+						</span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                    <input class="input100" type="password" name="password" placeholder="Password" required>
+                    <span class="focus-input100"></span>
+                    <span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+                </div>
+                <div class="text-right">
+                    <input type="checkbox" name="remember_me"> Remember me<small></small>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button class="login100-form-btn" type="submit" name="submitForm" value="Login">
+                        Login
+                    </button>
+                </div>
+                <div class="text-center p-t-136">
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/select2/select2.min.js"></script>
+<script src="vendor/tilt/tilt.jquery.min.js"></script>
+<script >
+    $('.js-tilt').tilt({
+        scale: 1.1
+    })
+</script>
+<script src="js/main.js"></script>
